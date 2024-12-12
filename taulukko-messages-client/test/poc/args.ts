93b86354b66e29ed5fdabc07b3f08954738a4374
gandb <gandnegro@gmail.com>
2024-12-12 13:39:12 +0000

    Remove duplicated tests

--- taulukko-messages-client/test/poc/args.ts
+++
@@ -1,8 +0,0 @@
-
-function args1(...args:any)
-{
-    console.log("args",args);
-    console.log("args",...args);
-}
-
-args1(1,2,3);
--- taulukko-messages/test/poc/taulukko-keytools.ts
+++
@@ -1,9 +0,0 @@
- 
-import { KeyTool } from "taulukko-commons";
-
-const keyTool: KeyTool = new KeyTool();
-
-
-console.log(keyTool);
-console.log(keyTool.build(1,1));
-
