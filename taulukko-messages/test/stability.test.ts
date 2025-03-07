deb76777670ccb258cfe543a3ade1f089c17305c
gandb <gandnegro@gmail.com>
2025-03-07 16:24:37 +0000

    remotion test invalid

--- taulukko-messages/test/stability.test.ts
+++ taulukko-messages/test/stability.test.ts
@@ -162,106 +162,6 @@ describe('#Stability test-', function  () {
  
   }); 
  
-  it('reconecting with a subscriber with problems',async  () => {
-    const NUMBER_OF_TESTS = 2;
-    let times = 0;
-   
-    const server = await initServer();
-
-    assert.equal(server.publishers.length,0);
-
-    let publisher = await Publisher.create({ 
-      server:"taulukko://localhost:" + DEFAULT_PORT,
-      topics:["topic.helloWorld"],
-      defaultLogLevel:LogLevel.ERROR
-    });
-
-    assert.equal(server.publishers.length,0,"Must be zero before the publisher.open");
-    assert.equal(publisher.data.status,serviceStatus.STARTING,"Must be STARTING before publisher.open");
-
-    await publisher.open();
-
-    assert.equal(publisher.data.status,serviceStatus.ONLINE,"Must be ONLINE after publisher.open");
-
-    assert.equal(server.publishers.length,1,"Must be 1 after publisher.open");
-
-    assert.equal(server.subscribers.length,0,"Must be 0 before subscriber.open");
-   let subscriber = await Subscriber.create({ 
-      server:"taulukko://localhost:" + DEFAULT_PORT,
-      topics:["topic.helloWorld","unexistentTopic" ], defaultLogLevel:LogLevel.ERROR
-    });
-   assert.equal(server.subscribers.length,0,"Must be 0 before subscriber.open");
-    assert.equal(server.publishers.length,1,"Must be 1 yet");
-
-    assert.equal(subscriber.data.status,serviceStatus.STARTING,"Must be STARTING before open");
-
-    await subscriber.open();
-
-    assert.equal(server.subscribers.length,1,"Must be 1 after open");
-
-    assert.equal(subscriber.data.status,serviceStatus.ONLINE,"Must be ONLINE before open");
- 
-    const onNewMessage = async (message:Message)=>{
-      try{
-      
-        assert.equal(message.topic,"topic.helloWorld","Topic need be the same topic in the publisher.send");
-        assert.equal(message.data, "Hello World","Message need be Hello World");
-      
-        
-        if(++times==NUMBER_OF_TESTS)
-        {
-         await subscriber.close();
-          await publisher.close(); 
-          await server.close(); 
-        }
-
-         
-      }catch(e){
-        lastError=e; 
-        await subscriber.forceClose();
-        await publisher.forceClose();
-        await server.forceClose();  
-      }
-      finally{ 
-
-        semaphore = true;
-      }
-     
-    };
-
-    await subscriber.on(onNewMessage); 
-
-    cleanupGlobals();
-    await publisher.send("Hello World"); 
-
- 
-    assert.ifError( await receiveTheMessage()); 
-   await subscriber.close(); 
- 
-    assert.equal(server.publishers.length,1,"Need keep 1 after subscriber close");
-    assert.equal(server.subscribers.length,0,"Must be one  after the subscriber.open");
-    assert.equal( subscriber.data.status,serviceStatus.STOPED,"Must be STOPED after  subscriber.close");
-   subscriber = await Subscriber.create({ 
-      topics:["topic.helloWorld","unexistentTopic" ], defaultLogLevel:LogLevel.ERROR
-    });
-   await subscriber.open();
-
-    await subscriber.on(onNewMessage); 
-   assert.equal(server.publishers.length,1,"Must be zero after the publisher.close");
-    assert.equal(server.subscribers.length,1,"Must be one  after the subscriber.open");
-    assert.equal(subscriber.data.status,serviceStatus.ONLINE,"Must be ONLINE  after  publisher.open");
-
-       
-    cleanupGlobals();
-   await publisher.send("Hello World"); 
-
-   assert.ifError( await receiveTheMessage());
-
-   assert.equal(server.subscribers.length,0,"Must be zero after the subscriber.close");
-    assert.equal(publisher.data.status,serviceStatus.STOPED,"Must be STOPED after  publisher.close");
-
- }); 
-  
  
  it('publish into a inexistent server with timeout but the server up before timeout',async function()
   {
@@ -361,6 +261,201 @@ const TIMEOUT = 10 * MINUTE;
 
 describe('#stability long time test-', function ()  {
   this.timeout(TIMEOUT);
+
+    
+    it('reconecting with a subscriber with problems',async  () => {
+      const NUMBER_OF_TESTS = 2;
+      let times = 0;
+    
+      const server = await initServer();
+
+      assert.equal(server.publishers.length,0);
+
+      let publisher = await Publisher.create({ 
+        server:"taulukko://localhost:" + DEFAULT_PORT,
+        topics:["topic.helloWorld"],
+        defaultLogLevel:LogLevel.ERROR
+      });
+
+      assert.equal(server.publishers.length,0,"Must be zero before the publisher.open");
+      assert.equal(publisher.data.status,serviceStatus.STARTING,"Must be STARTING before publisher.open");
+
+      await publisher.open();
+
+      assert.equal(publisher.data.status,serviceStatus.ONLINE,"Must be ONLINE after publisher.open");
+
+      assert.equal(server.publishers.length,1,"Must be 1 after publisher.open");
+
+      assert.equal(server.subscribers.length,0,"Must be 0 before subscriber.open");
+    let subscriber = await Subscriber.create({ 
+        server:"taulukko://localhost:" + DEFAULT_PORT,
+        topics:["topic.helloWorld","unexistentTopic" ], defaultLogLevel:LogLevel.ERROR
+      });
+    assert.equal(server.subscribers.length,0,"Must be 0 before subscriber.open");
+      assert.equal(server.publishers.length,1,"Must be 1 yet");
+
+      assert.equal(subscriber.data.status,serviceStatus.STARTING,"Must be STARTING before open");
+
+      await subscriber.open();
+
+      assert.equal(server.subscribers.length,1,"Must be 1 after open");
+
+      assert.equal(subscriber.data.status,serviceStatus.ONLINE,"Must be ONLINE before open");
+  
+      const onNewMessage = async (message:Message)=>{
+        try{
+        
+          assert.equal(message.topic,"topic.helloWorld","Topic need be the same topic in the publisher.send");
+          assert.equal(message.data, "Hello World","Message need be Hello World");
+        
+          
+          if(++times==NUMBER_OF_TESTS)
+          {
+          await subscriber.close();
+            await publisher.close(); 
+            await server.close(); 
+          }
+
+          
+        }catch(e){
+          lastError=e; 
+          await subscriber.forceClose();
+          await publisher.forceClose();
+          await server.forceClose();  
+        }
+        finally{ 
+
+          semaphore = true;
+        }
+      
+      };
+
+      await subscriber.on(onNewMessage); 
+
+      cleanupGlobals();
+      await publisher.send("Hello World"); 
+
+  
+      assert.ifError( await receiveTheMessage()); 
+    await subscriber.close(); 
+  
+      assert.equal(server.publishers.length,1,"Need keep 1 after subscriber close");
+      assert.equal(server.subscribers.length,0,"Must be one  after the subscriber.open");
+      assert.equal( subscriber.data.status,serviceStatus.STOPED,"Must be STOPED after  subscriber.close");
+    subscriber = await Subscriber.create({ 
+        topics:["topic.helloWorld","unexistentTopic" ], defaultLogLevel:LogLevel.ERROR
+      });
+    await subscriber.open();
+
+      await subscriber.on(onNewMessage); 
+    assert.equal(server.publishers.length,1,"Must be zero after the publisher.close");
+      assert.equal(server.subscribers.length,1,"Must be one  after the subscriber.open");
+      assert.equal(subscriber.data.status,serviceStatus.ONLINE,"Must be ONLINE  after  publisher.open");
+
+        
+      cleanupGlobals();
+    await publisher.send("Hello World"); 
+
+    assert.ifError( await receiveTheMessage());
+
+    assert.equal(server.subscribers.length,0,"Must be zero after the subscriber.close");
+      assert.equal(publisher.data.status,serviceStatus.STOPED,"Must be STOPED after  publisher.close");
+
+  }); 
+    
+  
+  it('reconecting with a publisher with problems',async  () => {
+    const NUMBER_OF_TESTS = 2;
+    
+    let times = 0;
+
+    const server:Server = await initServer();
+
+    assert.equal(server.publishers.length,0);
+
+    let publisher:Publisher = await Publisher.create({ 
+      server:"taulukko://localhost:" + DEFAULT_PORT,
+      topics:["topic.helloWorld"],
+      defaultLogLevel:LogLevel.ERROR
+    });
+
+    assert.equal(server.publishers.length,0,"Must be zero before the publisher.open");
+    assert.equal(publisher.data.status,serviceStatus.STARTING,"Must be STARTING before publisher.open");
+
+    await publisher.open();
+
+    assert.equal(publisher.data.status,serviceStatus.ONLINE,"Must be ONLINE after publisher.open");
+
+    assert.equal(server.publishers.length,1,"Must be 1 after publisher.open");
+
+    assert.equal(server.subscribers.length,0,"Must be 0 before subscriber.open");
+    let subscriber:Subscriber = await Subscriber.create({ 
+      server:"taulukko://localhost:" + DEFAULT_PORT,
+      topics:["topic.helloWorld","unexistentTopic" ], defaultLogLevel:LogLevel.ERROR
+    });
+    assert.equal(server.subscribers.length,0,"Must be 0 before subscriber.open");
+    assert.equal(server.publishers.length,1,"Must be 1 yet");
+
+    assert.equal(subscriber.data.status,serviceStatus.STARTING,"Must be STARTING before open");
+
+    await subscriber.open();
+
+    assert.equal(server.subscribers.length,1,"Must be 1 after open");
+
+    assert.equal(subscriber.data.status,serviceStatus.ONLINE,"Must be ONLINE before open");
+
+    const onNewMessage = async (message:Message)=>{
+      try{
+        assert.equal(message.topic,"topic.helloWorld","Topic need be the same topic in the publisher.send");
+        assert.equal(message.data, "Hello World","Message need be Hello World");
+       
+        if(++times===NUMBER_OF_TESTS)
+        {
+          
+          await subscriber.close();
+          await publisher.close();
+          await server.close();   
+        }
+      }catch(e){
+        lastError=e; 
+        await subscriber.forceClose();
+        await publisher.forceClose();
+        await server.forceClose();  
+      }
+      finally{ 
+
+        semaphore = true;
+      }
+      
+    };
+
+    await subscriber.on(onNewMessage); 
+
+    cleanupGlobals();
+    await publisher.send("Hello World"); 
+
+
+    assert.ifError( await receiveTheMessage()); 
+    await publisher.close(); 
+
+    assert.equal(server.publishers.length,0,"Need keep 0 after publisher close");
+    assert.equal(server.subscribers.length,1,"Must keep subscriber");
+    assert.equal( publisher.data.status,serviceStatus.STOPED,"Must be STOPED after publisher.close");
+    publisher = Publisher.create({ 
+      server:"taulukko://localhost:" + DEFAULT_PORT,
+      topics:["topic.helloWorld"],
+      defaultLogLevel:LogLevel.ERROR
+    }); 
+    await publisher.open(); 
+    cleanupGlobals(); 
+    await publisher.send("Hello World");  
+    assert.ifError( await receiveTheMessage()); 
+    assert.equal(server.publishers.length,0,"Must be zero after the subscriber.close");
+    assert.equal(publisher.data.status,serviceStatus.STOPED,"Must be STOPED after  publisher.close");
+
+  }); 
+
+
   it('Publisher and subscriber reconect after server restart',async  () => {
     const logger = Logger.create({});
     
@@ -519,6 +614,7 @@ describe('#stability long time test-', function ()  {
 
 //auxiliar functions
 function receiveTheMessage():Promise<Error|null> { 
+  
 
   const TIME_TO_CHECK = 50;
   return new Promise((resolve,reject)=>{ 
